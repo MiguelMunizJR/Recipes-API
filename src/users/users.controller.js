862f82cc7247cab81573017d0603e9f227101146
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2022-10-18 20:09:16 -0500

    cryptoJS

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,6 +1,36 @@
-const Users = require('../models/users.model');
+//? Dependencies
+const uuid = require("uuid");
+const Users = require("../models/users.models");
+const hashPass = require("../utils/crypto").hashPass;
 
-const getUsers = async () => {
+const getAllUsers = async () => {
   const data = await Users.findAll();
   return data;
+};
+
+const getUserById = async (id) => {
+  const data = await Users.findOne({
+    where: {
+      id: id,
+    },
+  });
+  return data;
+};
+
+const createUser = async (data) => {
+  const newUser = await Users.create({
+    id: uuid.v4(),
+    firstName: data.firstName,
+    lastName: data.lastName,
+    email: data.email,
+    password: hashPass(data.password),
+    phone: data.phone,
+    birthday: data.birthday,
+    gender: data.gender,
+    role: data.role,
+    country: data.country,
+    status: data.status,
+    isVerified: data.isVerified,
+  });
+  return newUser;
 };
